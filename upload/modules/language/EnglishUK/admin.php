a0ee6612639d197b4cb60c225b632891da26e7ec
WhenUHackUNoob <63111477+WhenUHackUNoob@users.noreply.github.com>
2021-01-01 16:18:02 +0000

    Update admin.php
    Updated a few stuff because party, you may be good with Norwegain however English hmmm. Not the best!

--- upload/modules/language/EnglishUK/admin.php
+++ upload/modules/language/EnglishUK/admin.php
@@ -28,20 +28,20 @@ $language = array(
 	'new_status' => 'New Status',
 	'creating_new_status' => 'Creating new status',
 	'status_name' => 'Status Name',
-	'status_html' => 'Status Html',
-	'confirm_delete_status' => 'Are you sure you want to delete this status?',
+	'status_html' => 'Status HTML',
+	'confirm_delete_status' => 'Please confirm that you want to delete this status!',
 	'marked_as_open' => 'Marked as open',
-	'status_created_successfully' => 'Status created successfully.',
-	'status_updated_successfully' => 'Status updated successfully.',
-	'status_deleted_successfully' => 'Status deleted successfully.',
+	'status_created_successfully' => 'Successfully created a new status..',
+	'status_updated_successfully' => 'Successfully updated the status.',
+	'status_deleted_successfully' => 'Sucessfully deleted the status.',
 	
 	// Permissions
 	'suggestions_manage' => 'StaffCP &raquo; Suggestions &raquo; Manage Settings',
 	
 	// Update Alert
 	'new_update_available_x' => 'There is a new update available for the module {x}',
-	'new_urgent_update_available_x' => 'There is a new urgent update available for the module {x}. Please update as soon as possible!',
+	'new_urgent_update_available_x' => 'There is an urgent updated needed for the {x} module.. Please update when you can!',
 	'current_version_x' => 'Current module version: {x}',
-	'new_version_x' => 'New module version: {x}',
+	'new_version_x' => 'Latest version: {x}',
 	'view_resource' => 'View Resource',
 );
