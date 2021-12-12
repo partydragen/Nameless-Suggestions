7fb13bdf872ecd18311d1bd2b92bf883ecb261a4
Enrico <41052284+enno123@users.noreply.github.com>
2021-12-12 12:13:59 +0100

    German translation added

---
+++ upload/modules/Suggestions/language/German/admin.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ *  Made by Partydragen translated by enno123
+ *  https://github.com/partydragen/Nameless-Suggestions
+ *  https://partydragen.com/
+ *
+ *  German Language for Suggestions module
+ */
+
+$language = array(
+    'suggestions_module' => 'Vorschläge Modul',
+    'settings' => 'Einstellungen',
+    'actions' => 'Aktionen',
+    'editing_x' => 'Bearbeitung von {x}', // Don't replace {x}
+    'link_location' => 'Link Standort',
+    'icon' => 'Icon',
+    'icon_example' => 'Beispiel: <i class="fa fa-users" aria-hidden="true"></i>',
+    'settings_updated_successfully' => 'Einstellungen erfolgreich aktualisiert.',
+    
+    'categories' => 'Kategorien',
+    'new_category' => 'Neue Kategorie',
+    'creating_new_category' => 'Neue Kategorie erstellen',
+    'category_name' => 'Kategorie Name',
+    'category_order' => 'Kategorie Reihenfolge',
+    'confirm_delete_category' => 'Bist du sicher das du die Kategorie löschen möchtest?',
+    'category_created_successfully' => 'Kategorie erfolgreich erstellt.',
+    'category_updated_successfully' => 'Kategorie erfolgreich aktualisiert.',
+    'category_deleted_successfully' => 'Kategorie erfolgreich gelöscht.',
+    
+    'statuses' => 'Status',
+    'status' => 'Status',
+    'new_status' => 'Neuer Status',
+    'creating_new_status' => 'Neuen Status erstellen',
+    'status_name' => 'Status Name',
+    'status_html' => 'Status Html',
+    'confirm_delete_status' => 'Bist du sicher das du den Status löschen möchtest?',
+    'marked_as_open' => 'Markiert als offen',
+    'status_created_successfully' => 'Status erfolgreich erstellt.',
+    'status_updated_successfully' => 'Status erfolgreich aktualisiert.',
+    'status_deleted_successfully' => 'Status erfolgreich gelöscht.',
+    
+    // Permissions
+    'suggestions_manage' => 'StaffCP &raquo; Vorschläge &raquo; Einstellungen verwalten',
+    
+    // Update Alert
+    'new_update_available_x' => 'Es gibt ein neues Update für das Modul {x}',
+    'new_urgent_update_available_x' => 'Es gibt ein neues dringendes Update für das Modul {x}. Bitte aktualisiere so schnell wie möglich!',
+    'current_version_x' => 'Aktuelle Modulversion: {x}',
+    'new_version_x' => 'Neue Modulversion: {x}',
+    'view_resource' => 'Ressource anzeigen',
+);
---
+++ upload/modules/Suggestions/language/German/general.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *  Made by Partydragen translated by enno123
+ *  https://github.com/partydragen/Nameless-Suggestions
+ *  https://partydragen.com/
+ *
+ *  German Language for Suggestions module
+ */
+
+$language = array(
+    'suggestions' => 'Vorschläge',
+    'no_suggestions' => 'Keine Vorschläge gefunden.',
+    'new_suggestion' => 'Neuer Vorschlag',
+    'none_statuses_defined' => 'Keine Status gefunden',
+    'none_categories_defined' => 'Keine Kategorien gefunden',
+    'title' => 'Titel',
+    'last_reply' => 'Letzte Antwort',
+    'stats' => 'Stats',
+    'likes' => 'Likes',
+    'dislikes' => 'Dislikes',
+    'categories' => 'Kategorien',
+    'category' => 'Kategorie',
+    'content' => 'Content',
+    'status' => 'Status',
+    'recent_activity' => 'Letzte aktivität',
+    'search_keyword' => 'Suchbegriff',
+    'sort_by' => 'Sortieren nach',
+    'newest' => 'Neuste',
+    'comments' => 'Kommentare',
+    'new_comment' => 'Neuer Kommentar',
+    'new_suggestion_comment' => 'Neuer Vorschlag Kommentar',
+    'editing_suggestion' => 'Vorschlag zur Bearbeitung',
+    'spam_wait' => 'Bitte warte {x} Sekunden, bevor du erneut postest',
+    
+    // Errors
+    'title_required' => 'Du musst ein Titel eingeben',
+    'content_required' => 'Du musst einen Inhalt eingeben',
+    'title_minimum' => 'Der Titel muss aus mindestens 6 Zeichen bestehen',
+    'content_minimum' => 'Der Inhalt muss aus mindestens 6 Zeichen bestehen',
+    'title_maximum' => 'Der Titel darf maximal 128 Zeichen lang sein',
+    'comment_minimum' => 'Der Kommentar muss aus mindestens 3 Zeichen bestehen',
+    'comment_maximum' => 'Der Kommentar darf maximal 10000 Zeichen lang sein',
+    'login_to_vote' => 'Du musst eingeloggt sein, um abstimmen zu können!',
+    
+    // Hook
+    'hook_new_suggestion' => 'Neuer Vorschlag von {x}',
+    'hook_new_comment' => 'Neuer Kommentar von {x} - Vorschlag hat {y} Likes und {z} Dislikes',
+);
