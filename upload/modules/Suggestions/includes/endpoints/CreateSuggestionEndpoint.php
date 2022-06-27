12dfd1433cda22d6740d846d7620b8a4dbebece0
Partydragen <admin@partydragen.com>
2022-06-27 22:29:56 +0200

    Add full suggestion info to create endpoint

--- upload/modules/Suggestions/includes/endpoints/CreateSuggestionEndpoint.php
+++ upload/modules/Suggestions/includes/endpoints/CreateSuggestionEndpoint.php
@@ -57,9 +57,44 @@ class CreateSuggestionEndpoint extends KeyAuthEndpoint {
         $suggestion = new Suggestion();
         $suggestion->create($user, $_POST['title'], nl2br($_POST['content']), $category_id);
 
+        $status = $api->getDb()->query('SELECT * FROM nl2_suggestions_statuses WHERE id = ?', [$suggestion->data()->status_id]);
+        $status = $status->first();
+
+        $category = $api->getDb()->query('SELECT * FROM nl2_suggestions_categories WHERE id = ?', [$suggestion->data()->category_id]);
+        $category = $category->first();
+
         $api->returnArray([
+            'id' => (int)$suggestion->data()->id,
             'suggestion_id' => (int)$suggestion->data()->id,
-            'link' => rtrim(Util::getSelfURL(), '/') . URL::build('/user/submissions/', 'view=' . Output::getClean($suggestion->data()->id))
+            'link' => rtrim(Util::getSelfURL(), '/') . URL::build('/user/submissions/', 'view=' . Output::getClean($suggestion->data()->id)),
+            'author' => [
+                'id' => (int)$suggestion->data()->user_id,
+                'username' => $user->getDisplayname(true),
+                'nickname' => $user->getDisplayname()
+            ],
+            'updated_by' => [
+                'id' => (int)$suggestion->data()->updated_by,
+                'username' => $user->getDisplayname(true),
+                'nickname' => $user->getDisplayname()
+            ],
+            'status' => [
+                'id' => (int)$suggestion->data()->status_id,
+                'name' => $status ? $status->name : 'Unknown',
+                'open' => $status ? ($status->open ? true : false) : false
+            ],
+            'category' => [
+                'id' => (int)$suggestion->data()->category_id,
+                'name' => $category ? $category->name : 'Unknown'
+            ],
+            'title' => Output::getClean($suggestion->data()->title),
+            'content' => Output::getDecoded($suggestion->data()->content),
+            'views' => (int)$suggestion->data()->views,
+            'created' => (int)$suggestion->data()->created,
+            'last_updated' => (int)$suggestion->data()->last_updated,
+            'likes_count' => (int)$suggestion->data()->likes,
+            'dislikes_count' => (int)$suggestion->data()->dislikes,
+            'likes' => [],
+            'dislikes' => [],
         ]);
     }
 
